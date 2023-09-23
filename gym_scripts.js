6c981bc888f25186560d038bd173c0a6b82f0aec
Mikenziokah <mikenziokah@gmail.com>
2023-09-23 23:48:39 +0300

    pic

--- gym_scripts.js
+++
@@ -1,6 +0,0 @@
-const images = ['image1.jpg', 'image2.jpg', 'image3.jpg']; // Add your image file names here
-const randomImage = images[Math.floor(Math.random() * images.length)];
-document.body.style.backgroundImage = url('pics/${randomImage}')`;
-document.body.style.backgroundRepeat = 'no-repeat';
-document.body.style.backgroundSize = 'cover';
-
